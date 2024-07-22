6234695de95d8ba3265834cdcc0dc0cb45cf09dd
Sangwonee <sangw12477@gmail.com>
2024-07-22 16:27:20 +0900

    first commit

---
+++ src/main/java/com/pj/planjourney/PlanJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.pj.planjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pj/planjourney/PlanJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.planjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
