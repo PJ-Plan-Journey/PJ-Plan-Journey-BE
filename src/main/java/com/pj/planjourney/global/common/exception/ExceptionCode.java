2c95a94a4d7bd53b3f64aae2dccfd02563f21d9a
Sangwonee <sangw12477@gmail.com>
2024-08-02 19:41:23 +0900

    fix : 친구 예외 메세지 수정

--- src/main/java/com/pj/planjourney/global/common/exception/ExceptionCode.java
+++ src/main/java/com/pj/planjourney/global/common/exception/ExceptionCode.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"회원를 찾을 수 없습니다."),
-    REQUEST_ALREADY_SENT(HttpStatus.BAD_REQUEST, "Friend request already sent"),
-    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "Friend request not found");
+    REQUEST_ALREADY_SENT(HttpStatus.BAD_REQUEST, "이미 보낸 친구 요청입니다."),
+    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "친구요청을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
