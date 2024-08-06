b96256015f760f7e5f0977cca91578229fcfe731
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-06 09:55:25 +0900

    fix : ActiveProfiles import 추가

--- src/test/java/com/pj/planjourney/domain/user/service/UserServiceTest.java
+++ src/test/java/com/pj/planjourney/domain/user/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
